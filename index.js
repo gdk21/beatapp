e5fa0d010f9e9d3a2b1b2875057fb5b9b500fff2
GDK 21 <g.denysiak21@gmail.com>
2019-09-11 09:34:01 +0200

    add all file

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', function(){
+  const sounds =  document.querySelectorAll('.sound');
+  const pads =  document.querySelectorAll('.pads div');
+  const visual = document.querySelector('.visual');
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#6860d3",
+    "#60b2d3"
+  ];
+
+
+  //Lire les sons
+  pads.forEach(function(pad, i){
+    pad.addEventListener('click',function(){
+      sounds[i].currentTime = 0;
+      sounds[i].play();
+
+      createBubbles(i);
+    });
+  });
+
+  //Animation des bulles
+  const createBubbles = function(i){
+    const bubble = document.createElement('div');
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[i];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener('animationend', function(){
+      visual.removeChild(this);
+    });
+  }
+});
